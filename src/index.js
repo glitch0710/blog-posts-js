f1f72fc1973317f98f7b3d9344f4c106b07108f9
Rhogiel Andoy <theglitch0710@gmail.com>
2022-03-09 17:03:19 +0800

    #1 First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import spidey1 from './image/spidey-1.jpg';
+import spidey2 from './image/spidey-2.png';
+import spidey3 from './image/spidey-3.jpg';
+import SingleComment from './SingleComment';
+import UserCard from './UserCard';
+
+const App = () => {
+    return(
+        <div className='ui comments'>
+            <UserCard>
+              <SingleComment
+                name='Peter 1'
+                date='Today at 4:30PM'
+                comment='Wow! This is amazing. React is amazing!'
+                picture={spidey1}
+              />
+            </UserCard>
+            <UserCard>
+              <SingleComment
+                name='Peter 2'
+                date='Today at 9:30PM'
+                comment='Hello! Good day :)'
+                picture={spidey2}
+              />
+            </UserCard>
+            <UserCard>
+              <SingleComment
+                name='Peter 3'
+                date='Today at 10:00AM'
+                comment='ReactJS is indeed incredible!'
+                picture={spidey3}
+              />
+            </UserCard>
+        </div>
+    )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
